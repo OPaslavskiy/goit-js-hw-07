6bbe91df0854a873cd2531d21e582aba7d0c9c01
OPaslavskiy <bankir.op@gmail.com>
2022-12-07 23:48:49 +0200

    2 ready

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(`.gallery`);
+makeMarkupForGallary();
+
+function makeMarkupForGallary() {
+  const markup = galleryItems.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>`,
+    ""
+  );
+  gallery.insertAdjacentHTML("beforeend", markup);
+}
+
+// const slider = new SimpleLightbox(".gallery .gallery__item", {
+//   captionsData: "alt",
+//   captionDelay: 250,
+// });
