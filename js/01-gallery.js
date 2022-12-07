ff8b7c1511cdc3f0771821326f316f7336b0a8a8
OPaslavskiy <bankir.op@gmail.com>
2022-12-07 22:31:41 +0200

    tansk num1 (not esc)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(`.gallery`);
+gallery.addEventListener("click", showBigSizePhoto);
+makeMarkupForGallary();
+
+function makeMarkupForGallary() {
+  const markup = galleryItems.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                data-source="${original}"
+            />
+        </a>
+    </div>`,
+    ""
+  );
+  gallery.insertAdjacentHTML("beforeend", markup);
+}
+
+function showBigSizePhoto(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  let currentBigPhoto = evt.target.dataset.source;
+
+  const bigPhoto = basicLightbox.create(`
+    <img src="${currentBigPhoto}" width="800" height="600">
+`);
+  bigPhoto.show();
+}
